a3d724f8f71527e57abb27fa5735946bc1ed4758
Wes Harrison <wesharrison89@gmail.com>
2023-03-04 21:16:54 -0800

    added mock readme

--- genmkd/generateMarkdown.js
+++ genmkd/generateMarkdown.js
@@ -32,7 +32,16 @@ function renderLicenseBadge(license) {
   
   // TODO: Create a function that returns the license section of README
   // If there is no license, return an empty string
-  function renderLicenseSection(license) {}
+  function renderLicenseSection(license) {
+    if (license) {
+      return `
+      License
+      This repository is licensed by the ${renderLicenseLink(license)} license.
+      `
+    } else {
+      return '';
+    }
+  }
   
   // TODO: Create a function to generate markdown for README
   function generateMarkdown(data) {
